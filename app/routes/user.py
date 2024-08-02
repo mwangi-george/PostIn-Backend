50ea50db21528fc2e43ac97c0aa256942612a338
George Mwangi <mwangigeorge648@gmail.com>
2024-08-02 14:54:08 +0300

    Added routes

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
-from pydantic import EmailStr
 from sqlalchemy.orm import Session
 from app.schemas import UserCreate, ActionConfirm, Token
 from app.services import UserServices
@@ -15,7 +14,7 @@ def create_user_routes() -> APIRouter:
 
     user_services = UserServices()
 
-    @router.post('/user', response_model=ActionConfirm, status_code=status.HTTP_201_CREATED)
+    @router.post('/new', response_model=ActionConfirm, status_code=status.HTTP_201_CREATED)
     async def create_user(user_profile: UserCreate, db: Session = Depends(get_db)):
         msg = user_services.create_user(user_profile, db)
         formatted_msg = ActionConfirm(msg=msg)
