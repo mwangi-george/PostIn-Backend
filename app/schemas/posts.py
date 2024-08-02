a1096dc7ceff2da77fcc3337b0afb5b0e277e167
George Mwangi <mwangigeorge648@gmail.com>
2024-08-02 14:53:06 +0300

    Added pydantic models to validate outputs and inputs

--- app/schemas/posts.py
+++ app/schemas/posts.py
@@ -10,3 +10,23 @@ class PostCreate(BaseModel):
         from_attributes = True
 
 
+class Post(BaseModel):
+    post_id: int
+    content: str
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
+        json_schema_extra = {
+            "example": {
+                "post_id": 1,
+                "content": "We love fastapi",
+                "created_at": "2020-08-02 13:24:46.281204",
+                "updated_at": "2020-08-02 13:24:46.281204"
+            }
+        }
+
+
+class ManyPosts(BaseModel):
+    posts: list[Post] | None
