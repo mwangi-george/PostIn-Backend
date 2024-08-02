7b2d24d43ae1f254ae0190ae392146e134046fcd
George Mwangi <mwangigeorge648@gmail.com>
2024-08-02 14:53:40 +0300

    Added utility to build routes

--- app/services/posts.py
+++ app/services/posts.py
@@ -2,7 +2,8 @@ from datetime import datetime, timezone
 
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from models import User, Posts
+from sqlalchemy import and_
+from models import Posts
 from app.schemas import PostCreate
 
 
@@ -50,3 +51,43 @@ class PostServices:
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
 
             )
+
+    @staticmethod
+    def delete_post(post_id: int, user_id: int, db: Session) -> str:
+        db_post = db.query(Posts).filter(
+            and_(Posts.post_id == post_id, Posts.user_id == user_id)
+        ).first()
+
+        if db_post is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Post with id {post_id} does not exist"
+            )
+        try:
+            db.delete(db_post)
+            db.commit()
+            return f"Post with id {db_post.post_id} deleted"
+        except Exception as e:
+            print(e)
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="could not delete the post"
+            )
+
+    @staticmethod
+    def get_a_post(post_id: int, user_id: int, db: Session):
+        db_post = db.query(Posts).filter(
+            and_(Posts.post_id == post_id, Posts.user_id == user_id)
+        ).first()
+
+        if db_post is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Post with id {post_id} does not exist"
+            )
+        return db_post
+
+    @staticmethod
+    def get_all_posts(user_id: int, db: Session):
+        db_posts = db.query(Posts).filter(Posts.user_id == user_id).all()
+        return db_posts
