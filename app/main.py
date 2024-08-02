31bfe11f2e6f4f357d5d42a5357bf3c3e7f96c71
George Mwangi <mwangigeorge648@gmail.com>
2024-08-02 13:36:41 +0300

    Main Application entrypoint

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 # This is the main entry point of the application
 
 from fastapi import FastAPI
-from app.routes import user_routes
+from app.routes import user_routes, posts_routes
 
 
 def create_app() -> FastAPI:
     server = FastAPI()
 
     user_router = user_routes()
+    posts_router = posts_routes()
     server.include_router(user_router)
+    server.include_router(posts_router)
     return server
 
 
